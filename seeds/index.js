df6b5b14cf0520ea9a149ea1dc65aac0ee25e16f
Renato Boucas <renatoboucas@gmail.com>
2022-04-12 01:18:12 -0400

    Add cluster map, change cluster size and color, add custom popup

--- seeds/index.js
+++ seeds/index.js
@@ -18,18 +18,21 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
+    for(let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author: '624c5623ba90f848b2cdbc60',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum blanditiis nam sapiente et temporibus sed consequuntur, sunt tenetur voluptatem eum. Consequuntur officiis, numquam nemo repellendus iste tenetur illo repudiandae harum?',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
             geometry: {
-                type: 'Point',
-                coordinates: [-113.1331, 47.0202]
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
             },
             images: [
                 {
