100d5f7e401b424a1fe5708131edfb966108d36a
Renato Boucas <renatoboucas@gmail.com>
2022-04-14 02:27:46 -0400

    new app

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,19 @@
 const User = require('../models/user');
 
 module.exports.renderRegister = (req, res) => {
-    res.render('users/register')
+    res.render('users/register');
 }
 
-module.exports.register = async (req, res) => {
+module.exports.register = async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
         req.login(registeredUser, err => {
-            if(err) return next(err)
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
         })
-        req.flash('success', 'Welcome to Yelp Camp!');
-        res.redirect('/campgrounds');
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
@@ -21,11 +21,11 @@ module.exports.register = async (req, res) => {
 }
 
 module.exports.renderLogin = (req, res) => {
-    res.render('users/login')
+    res.render('users/login');
 }
 
 module.exports.login = (req, res) => {
-    req.flash('success', 'Welcome back!');
+    req.flash('success', 'welcome back!');
     const redirectUrl = req.session.returnTo || '/campgrounds';
     delete req.session.returnTo;
     res.redirect(redirectUrl);
@@ -33,6 +33,7 @@ module.exports.login = (req, res) => {
 
 module.exports.logout = (req, res) => {
     req.logout();
-    req.flash('success', 'Goodbye!');
+    // req.session.destroy();
+    req.flash('success', "Goodbye!");
     res.redirect('/campgrounds');
 }
