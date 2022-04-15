39429b0ae27a458c17ba90326b917efed1c4d974
Renato Boucas <renatoboucas@gmail.com>
2022-04-14 22:52:52 -0400

    testing if app is reading .env

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const MongoDBStore = require("connect-mongo")(session);
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'; 
 console.log(process.env.DB_URL);
+console.log(process.env.CLOUDINARY_SECRET);
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
